30ed990c063e78f74abfbe292bcfe7560fcc4899
Antonescu Alex <alx2004mc@gmail.com>
2026-08-10 19:39:49 +0300

    Created front-end project + Added Login Page

---
+++ ToDoAppFrontend/src/MainPage.jsx
@@ -0,0 +1,14 @@
+import "./MainPage.css";
+
+export default function MainPage() {
+  return (
+    <>
+      <div className="contents">
+        <div className="title-div">
+          <h className="my-lists-text">My Lists</h>
+        </div>
+        <div className="separation-line"></div>
+      </div>
+    </>
+  );
+}
---
+++ ToDoAppFrontend/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import LoginPage from "./LoginPage.jsx";
+import MainPage from "./MainPage.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <MainPage />
+  </StrictMode>,
+);
