92ef585169985e173b1557b40db9ccac2a343cc1
Antonescu Alex <alx2004mc@gmail.com>
2026-08-21 13:33:22 +0300

    Removed Docker Compose configuration. Initalised Spring Boot backend. Initialized Docker file within Spring Boot

---
+++ ToDoAppBackend/ToDoAppBackend/src/main/java/alex/antonescu/todoappbackend/ToDoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package alex.antonescu.todoappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoAppBackendApplication.class, args);
+    }
+
+}
---
+++ ToDoAppBackend/ToDoAppBackend/src/test/java/alex/antonescu/todoappbackend/ToDoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.antonescu.todoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
